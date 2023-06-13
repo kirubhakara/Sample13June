34647ea081f0e5f0302fb3e49ea86cdb9e5e0cca
kirubakaaran <karankiru1011@gmail.com>
2023-06-13 16:35:23 +0530

    my code

--- src/org/testJune/Sample13.java
+++ src/org/testJune/Sample13.java
@@ -2,13 +2,13 @@ package org.testJune;
 
 public class Sample13 {
 	
+	public  void demo() {
+		int a =10;
+		System.out.println(a);
+	}
+	
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
+		System.out.println("git project");
 	}
 
 }
