7c4a54b460db071a111babba0c8dc3240d1f69ff
kirubakaaran <karankiru1011@gmail.com>
2023-06-13 16:41:33 +0530

    karan

--- src/org/testJune/Sample13.java
+++ src/org/testJune/Sample13.java
@@ -6,6 +6,9 @@ public class Sample13 {
 		int a =10;
 		System.out.println(a);
 	}
+	public void sample() {
+		System.out.println("done");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("git project");
